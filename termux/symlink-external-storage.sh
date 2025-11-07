8007df20efa3d4ee54871261b698aaa9d3edf933
maximilionus <maximilionuss@gmail.com>
2025-11-07 05:01:49 +0300

    termux: Use more correct dir naming for external

--- termux/symlink-external-storage.sh
+++ termux/symlink-external-storage.sh
@@ -3,13 +3,14 @@
 set -e
 
 STORAGE="$HOME/storage"
-EXTERNAL_PRIVATE="$STORAGE/external-1"
-EXTERNAL_ROOT="$STORAGE/external-1-root"
+EXTERNAL_NAME="external-1"
+EXTERNAL_PRIVATE="$STORAGE/$EXTERNAL_NAME"
+EXTERNAL_SHARED="$STORAGE/shared-$EXTERNAL_NAME"
 
 if [[ ! -d $STORAGE ]]; then
     echo "Storage directory non-existent. Run 'termux-setup-storage' first."
     exit 1
 fi
 
-ln -s $(readlink -f "$EXTERNAL_PRIVATE" | cut -d/ -f-3) "$EXTERNAL_ROOT"
-echo "External storage is successfully symlinked to '$EXTERNAL_ROOT'."
+ln -s $(readlink -f "$EXTERNAL_PRIVATE" | cut -d/ -f-3) "$EXTERNAL_SHARED"
+echo "External storage is successfully symlinked to '$EXTERNAL_SHARED'."
